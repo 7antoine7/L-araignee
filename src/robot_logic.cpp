5387177e1f20398754672ea5491cedf7294ae25f
Mike <goufi_troop@hotmail.com>
2020-02-19 16:26:00 -0500

    SingleLeg ameliore Les pattes montent et descendent quand elles sont selectionnees
     On branch String_Publish
     Your branch is up to date with 'origin/String_Publish'.
    
     Changes to be committed:
    	modified:   src/robot_logic.cpp

--- src/robot_logic.cpp
+++ src/robot_logic.cpp
@@ -91,6 +91,7 @@ int prevButton = 0;
 int modeAuto = 0;
 int etape = 0;
 int animation = 0;
+int singleLegI = 0;
 float testAnalog = 0.0f;
 float testAnalog2 = 0.0f;
 float distanceTest_ = 0.0f;
@@ -193,14 +194,25 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 		if(tabBoutons[boutonBack])
 		{
 			startServos();
-			positionHanche = 1500;
-  			positionGenou = 1500;
-  			positionPatte = 1500;
+			singleLegI = 0;			
   			aEnvoye = true;
 			noPatte++;
 			if(noPatte > nbPattes)
 			{
 				noPatte = 1;	//Revient a 1 apres 6
+			}
+
+			if((noPatte <= 3) && (noPatte >= 1))
+			{
+				positionHanche = 1500;
+	  			positionGenou = 1850;
+	  			positionPatte = 1500;
+			}
+			else if((noPatte <= 6) && (noPatte >= 4))
+			{
+				positionHanche = 1500;
+	  			positionGenou = 1150;
+	  			positionPatte = 1500;
 			}
 		}
 		if(tabBoutons[boutonStart])	
@@ -239,7 +251,6 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
  */
 int main(int argc, char **argv)
 {
-	int singleLegI = 0;
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line. For programmatic
@@ -335,6 +346,16 @@ ros::Subscriber subJoy   = n.subscribe("joy", 10, joyCallback);
 
     	case singleLeg:
     		{
+    			singleLegI++;
+
+    			if(singleLegI == 15)
+    			{
+    				positionHanche = 1500;
+    				positionGenou = 1500;
+    				positionPatte = 1500;
+    				aEnvoye = true;
+    			}
+
     			if(tabAxes[axeHorizJoyGauche] != 0)
 				{
 					positionHanche = positionHanche - (tabAxes[axeHorizJoyGauche]*10);
@@ -383,9 +404,9 @@ ros::Subscriber subJoy   = n.subscribe("joy", 10, joyCallback);
 					aEnvoye = true;
 				}
 
-				if((tabAxes[croixVert] != 0) && (noPatte <= 3) && (noPatte >= 1))
+				if((tabAxes[axeVertJoyDroit] != 0) && (noPatte <= 3) && (noPatte >= 1))
 				{
-					positionPatte = positionPatte - (tabAxes[croixVert]*10);
+					positionPatte = positionPatte - (tabAxes[axeVertJoyDroit]*10);
 
 					if(positionPatte <= 750)
 					{
@@ -399,9 +420,9 @@ ros::Subscriber subJoy   = n.subscribe("joy", 10, joyCallback);
 					aEnvoye = true;
 				}
 
-				else if((tabAxes[croixVert] != 0) && (noPatte <= 6) && (noPatte >= 4))
+				else if((tabAxes[axeVertJoyDroit] != 0) && (noPatte <= 6) && (noPatte >= 4))
 				{
-					positionPatte = positionPatte + (tabAxes[croixVert]*10);
+					positionPatte = positionPatte + (tabAxes[axeVertJoyDroit]*10);
 
 					if(positionPatte <= 750)
 					{
